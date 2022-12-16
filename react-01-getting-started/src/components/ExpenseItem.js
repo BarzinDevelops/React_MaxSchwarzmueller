d28aab429a24828fabae9c981ce3910070be24b3
Barzin <barzin.development@gmail.com>
2022-12-16 15:45:57 +0100

    Part 4: React - Section3: => Passing data via 'props'.

--- react-01-getting-started/src/components/ExpenseItem.js
+++ react-01-getting-started/src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-const ExpenseItem = () =>{
-    const dt = new Date();
+const ExpenseItem = (props) =>{
     return(
         <div className='expense-item'>
                 <div className='date-item'>
-                    {dt.toLocaleDateString('nl-NL', 
-                    { day:"numeric", month:"long", year: 'numeric'})}
+                    {props.date.toLocaleDateString('nl-NL', {day: 'numeric', month: 'long', year: 'numeric'})}
                 </div>
 
             <div  className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>€294.67</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>€{props.price}</div>
             </div>
         </div>
 
