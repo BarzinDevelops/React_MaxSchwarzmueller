d1b62f17fad7caccde8f960556983809c702ac1c
Barzin <barzin.development@gmail.com>
2022-12-16 16:02:14 +0100

    Part 4: React - Section3: => Adding normal logic to component (for date to show day, month and year).

--- react-01-getting-started/src/components/ExpenseItem.js
+++ react-01-getting-started/src/components/ExpenseItem.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>{
+    const day = props.date.toLocaleString('nl-NL', {day: '2-digit'});
+    const month = props.date.toLocaleString('nl-NL', {month: 'short'});
+    const year =props.date.getFullYear();
+    
     return(
         <div className='expense-item'>
                 <div className='date-item'>
-                    {props.date.toLocaleDateString('nl-NL', {day: 'numeric', month: 'long', year: 'numeric'})}
+                   <div>{day}</div>
+                   <div>{month}</div>
+                   <div>{year}</div>
                 </div>
 
             <div  className='expense-item__description'>
