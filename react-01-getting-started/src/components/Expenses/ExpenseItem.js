7382bcbed6d4a8d90b0aa785d713609702e0e623
Barzin <barzin.development@gmail.com>
2022-12-18 13:53:25 +0100

    Section 4: React State & Working with Events => Adding Form and handeling user inputs by saving to state (two approaches: 1. each state seperately, 2. on state object that stores each input state as a property). Also clearing input fields after form submit event (here, you can create new object where each value saved in state can be stored for later use). Use two-way binding -> which means your return the state of each input element in the form by adding 'value={state}' property to that element.

--- react-01-getting-started/src/components/Expenses/ExpenseItem.js
+++ react-01-getting-started/src/components/Expenses/ExpenseItem.js
@@ -4,18 +4,14 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) =>{
-    // console.log(`props in ExpenseItem.js: `, props)
     const [title, setTitle] = useState(props.title)
-    const clickHandler = (e) => {
-        setTitle(()=> e.target.innerHTML)
-    }
+    
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div  className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>€{props.price}</div>
-                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
 
