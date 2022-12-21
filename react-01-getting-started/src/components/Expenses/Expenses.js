75c88e298bff7a846d8bedd01efe5436d18473a4
Barzin <barzin.development@gmail.com>
2022-12-21 17:38:47 +0100

    Section 4: React State & Working with Events => Filter the expenses based on chosen year, without changing any state! (conditional rendering using .map() or .filter() or both...).

--- react-01-getting-started/src/components/Expenses/Expenses.js
+++ react-01-getting-started/src/components/Expenses/Expenses.js
@@ -5,28 +5,51 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filteredExpenseState, setFilteredExpenseState] = useState("2019");
+  const [filteredExpenseYear, setFilteredExpenseYear] = useState("2020");
 
-  const filterExpenseHandler = (filteredExpense) => {
-    setFilteredExpenseState(filteredExpense);
+  const filterExpenseHandler = (filteredYear) => {
+    setFilteredExpenseYear(filteredYear);
   };
 
   return (
     <div>
+        {/* {} */}
       <Card className="expenses">
         <ExpensesFilter
-          defaultChosenYear={filteredExpenseState}
+          defaultChosenYear={filteredExpenseYear}
           onFilterExpense={filterExpenseHandler}
         />
-        {props.expenses.map((expense) => (
-          <ExpenseItem 
-            key={expense.id}
-            id={expense.id}
-            date={expense.date}
-            title={expense.title}
-            price={expense.amount}
-          />
-        ))}
+        {/* {    
+            props.expenses.map((expense) => {
+               return expense.date.getFullYear() === Number(filteredExpenseYear) &&
+                <ExpenseItem 
+                    key={expense.id}
+                    id={expense.id}
+                    date={expense.date}
+                    title={expense.title}
+                    price={expense.amount}
+                />
+                
+            })
+        } */}
+        {/* {console.log('props.expenses', props.expenses)} */}
+        {    
+            props.expenses.map((expense) => 
+               
+                <ExpenseItem 
+                    key={expense.id}
+                    id={expense.id}
+                    date={expense.date}
+                    title={expense.title}
+                    price={expense.amount}
+                />
+                
+            ).filter(expense => expense.props.date.getFullYear() === Number(filteredExpenseYear))
+        }
+        {/* {    
+            props.expenses.filter((expense) =>  [expense.date.getFullYear() === Number(filteredExpenseYear)] )
+        } */}
+
 
       </Card>
     </div>
