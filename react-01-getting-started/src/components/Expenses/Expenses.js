f12c0f3f9e6d6edc76a6ebec16f643ae0f601ca2
Barzin <barzin.development@gmail.com>
2022-12-21 14:30:03 +0100

    Section 4: React State & Working with Events => Get items in expenses array dynamically by looping through the array(map). After that make sure each new item gets added to existing list by useing state and appending each new item to previous existing list of expensis.

--- react-01-getting-started/src/components/Expenses/Expenses.js
+++ react-01-getting-started/src/components/Expenses/Expenses.js
@@ -1,49 +1,36 @@
-import React, {useState} from 'react';
-import './Expenses.css';
+import React, { useState } from "react";
+import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-    const [filteredExpenseState, setFilteredExpenseState] = useState('2019');
+  const [filteredExpenseState, setFilteredExpenseState] = useState("2019");
 
-    const filterExpenseHandler = (filteredExpense) => {       
-        setFilteredExpenseState(filteredExpense);
-    };
+  const filterExpenseHandler = (filteredExpense) => {
+    setFilteredExpenseState(filteredExpense);
+  };
 
-    return (
-        <div>
-            <Card className='expenses'>
-                <ExpensesFilter defaultChosenYear={filteredExpenseState} onFilterExpense={filterExpenseHandler}/>
-                { console.log('In Expenses.js => ', filteredExpenseState) }
-                <ExpenseItem 
-                    id={props.expenses[0].id}
-                    date={props.expenses[0].date}
-                    title={props.expenses[0].title}
-                    price={props.expenses[0].amount}
-                />
-                <ExpenseItem 
-                    id={props.expenses[1].id}
-                    date={props.expenses[1].date}
-                    title={props.expenses[1].title}
-                    price={props.expenses[1].amount}
-                />
-                <ExpenseItem 
-                    id={props.expenses[2].id}
-                    date={props.expenses[2].date}
-                    title={props.expenses[2].title}
-                    price={props.expenses[2].amount}
-                />
-                <ExpenseItem 
-                    id={props.expenses[3].id}
-                    date={props.expenses[3].date}
-                    title={props.expenses[3].title}
-                    price={props.expenses[3].amount}
-                />
-            </Card>
-        </div>
-        
-    )
-}
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          defaultChosenYear={filteredExpenseState}
+          onFilterExpense={filterExpenseHandler}
+        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem 
+            key={expense.id}
+            id={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.amount}
+          />
+        ))}
+
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
