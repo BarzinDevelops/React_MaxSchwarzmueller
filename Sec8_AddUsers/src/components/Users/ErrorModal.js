4efd90ca951e43ebae8fc184440f71d5ad2b2020
Barzin <barzin.development@gmail.com>
2022-12-26 15:38:42 +0100

    Section 8: Building Add User App => Create both components (Backdrop and ModalOverlay) within the ErrorModal component the current backdrop div and <Card /> component with the 2 just created components in the ErrorModal component. Therefore import react-dom package and use the 'createPortal' function to set a portal to the previous created (backdrop-root and overlay-root elements) by passing the component (with their props) and the DOM selection of the elements id (e.g. document.getElementById('backdrop-root').

--- Sec8_AddUsers/src/components/Users/ErrorModal.js
+++ Sec8_AddUsers/src/components/Users/ErrorModal.js
@@ -1,4 +1,5 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 import classes from "./ErrorModal.module.css";
 
@@ -6,9 +7,12 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const ErrorModal = (props) => {
-  return (
-    <Fragment>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+  };
+
+  const ModalOverlay = (props) => {
+    return (
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -20,6 +24,23 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
+    );
+  };
+
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </Fragment>
   );
 };
