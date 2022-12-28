143dc671179c91308db65ba8ce6457035a7a4cec
Barzin <barzin.development@gmail.com>
2022-12-28 21:34:39 +0100

    Section 10: useReducer => setup desctructured aliasses for isValid property of emailState and passwordState so it can be uses as dependencies in useEffect. This way the useEffect will only be executed when these validation dependencies are used. This is a performance enhencer where useEffect is only used as often as it needs to check for validity and once valid it won't execute, dispite of how many chages is done to input (as long validity isn't changed by it).

--- Sec10_usereducer/src/components/Login/Login.js
+++ Sec10_usereducer/src/components/Login/Login.js
@@ -42,19 +42,24 @@ const Login = (props) => {
     isValid: null,
   });
 
-  useEffect(() => {
-    console.log('EFFECT RUNNING');
+  // useEffect(() => {
+  //   console.log('EFFECT RUNNING');
 
-    return () => {
-      console.log('EFFECT CLEANUP');
-    };
-  }, []);
+  //   return () => {
+  //     console.log('EFFECT CLEANUP');
+  //   };
+  // }, []);
+
+
+  // create aliasses of the isValid property of both emailState an passwordState by using destructuring:
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid} = passwordState;
 
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(
-        emailState.isValid && passwordState.isValid
+        emailIsValid && passwordIsValid
       );
     }, 500);
 
@@ -62,7 +67,7 @@ const Login = (props) => {
       console.log('CLEANUP');
       clearTimeout(identifier);
     };
-  }, [emailState, passwordState]);
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
