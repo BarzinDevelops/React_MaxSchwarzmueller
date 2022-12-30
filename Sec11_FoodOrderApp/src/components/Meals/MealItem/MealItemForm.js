3e9b696353039b0158e961cbb57573707f177004
Barzin <barzin.development@gmail.com>
2022-12-30 14:28:36 +0100

    Section 11: Food Order App => Setup useRef to get amount value user provides from Input.js and receive this in MealItemForm. Then this value can be validated and forwarded through props.onAddToCart() which is set in MealItem.js component. (input field for amount). In there use the context to pass the needed item to reducer by returning (id, name, amount and price) via addItem method of carContext.

--- Sec11_FoodOrderApp/src/components/Meals/MealItem/MealItemForm.js
+++ Sec11_FoodOrderApp/src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           type: "number",
-          id: "amount_"+ props.id,
+          id: "amount_" + props.id,
           min: "1",
           max: "5",
           step: "1",
@@ -18,6 +40,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1 -5)</p>}
     </form>
   );
 };
