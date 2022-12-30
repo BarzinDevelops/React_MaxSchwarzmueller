a63e0bd0b2fafbecdfcc5758c03f82e12b5eff9a
Barzin <barzin.development@gmail.com>
2022-12-30 11:34:22 +0100

    Section 11: Food Order App => Create a state that manages when the cart is closed/open. Also setup 2 handlers(to show and hide) that can be triggered, which then change cartIsShown state. By passing the handlers to Modal and HeaderButton through prop drilling, the cart is able to be closed by eighter clicking 'close'-button or by clicking on 'backdrop'.

--- Sec11_FoodOrderApp/src/App.js
+++ Sec11_FoodOrderApp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Cart from './components/Cart/Cart';
 import Header from '../src/components/Layout/Header';
@@ -6,10 +6,19 @@ import Meals from './components/Meals/Meals';
 
 
 function App() {
+  const [cartIsSown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () =>{
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () =>{
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsSown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
