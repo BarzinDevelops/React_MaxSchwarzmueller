e063ea9d80e6bee335523bfad17c2b2e9f69227a
Barzin <barzin.development@gmail.com>
2022-12-31 16:04:17 +0100

    Section 11: Food Order App => Add useEffect to add a new animation class to Header button (which bumps every time a new item is added). Also adjust current styles of header and the header button, cart items, price etc, so it all is more responsive.

--- Sec11_FoodOrderApp/src/components/Layout/HeaderCartButton.js
+++ Sec11_FoodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,11 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   // console.log('props from HeaderCartButton ->', cartCtx)
 
@@ -12,13 +13,34 @@ const HeaderCartButton = (props) => {
     return currNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timerIdentifier = setTimeout(()=>{
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timerIdentifier);
+    };
+
+
+  }, [items]);
+
+
   return (
     <Fragment>
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
-        <span>Your Cart</span>
+        <span className={classes.title}>Your Cart</span>
         <span className={classes.badge}>{numberOfCartItems}</span>
       </button>
     </Fragment>
