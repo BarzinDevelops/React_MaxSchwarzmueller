8f386df29b5d47f7d17bcb5cdffebd21ea672e2a
Barzin <barzin.development@gmail.com>
2023-01-02 12:38:10 +0100

    Section 14: Connecting with API's (Databases) => Use fetch api to send http request to starwars backend api to get movies from this endpoint (url: 'https://swapi.dev/api/films'). Also create new object from the data received from the api, so that the property names match the property names used in the Movie component (which is used in MoviesList component that passes this data from App.js).

--- Sec14_HttpRequestToDatabase/src/App.js
+++ Sec14_HttpRequestToDatabase/src/App.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import Movie from './components/Movie';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = () =>{
+    fetch('https://swapi.dev/api/films')
+      .then(response=> {
+        const moviesData = response.json();
+        return moviesData;
+      })
+      .then( moviesData => {
+        const transformedMovies = moviesData.results.map( movie => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            releaseDate: movie.release_date,
+            openingText: movie.opening_crawl
+          }
+        })
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
