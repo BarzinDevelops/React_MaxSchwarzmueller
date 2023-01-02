a5f6e4c8ce9b42689d87dc38da24c36e1c05d8ef
Barzin <barzin.development@gmail.com>
2023-01-02 12:46:07 +0100

    Section 14: Connecting with API's (Databases) => Refactor fetchMoviesHandler function to fetch data using asyn/await for promisis returned, instead of then() methods. This will improve code readability and is less code. Test to check if the results are the same.

--- Sec14_HttpRequestToDatabase/src/App.js
+++ Sec14_HttpRequestToDatabase/src/App.js
@@ -7,23 +7,37 @@ import Movie from './components/Movie';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () =>{
-    fetch('https://swapi.dev/api/films')
-      .then(response=> {
-        const moviesData = response.json();
-        return moviesData;
-      })
-      .then( moviesData => {
-        const transformedMovies = moviesData.results.map( movie => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            releaseDate: movie.release_date,
-            openingText: movie.opening_crawl
-          }
-        })
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async () =>{
+    const response = await fetch('https://swapi.dev/api/films');
+    const moviesData = await response.json();
+  
+    const transformedMovies = moviesData.results.map( movie => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        releaseDate: movie.release_date,
+        openingText: movie.opening_crawl
+      }
+    })
+    setMovies(transformedMovies);
+
+    // Getting same data but using .then() method approach:
+    // fetch('https://swapi.dev/api/films')
+    //   .then(response=> {
+    //     const moviesData = response.json();
+    //     return moviesData;
+    //   })
+    //   .then( moviesData => {
+    //     const transformedMovies = moviesData.results.map( movie => {
+    //       return {
+    //         id: movie.episode_id,
+    //         title: movie.title,
+    //         releaseDate: movie.release_date,
+    //         openingText: movie.opening_crawl
+    //       }
+    //     })
+    //     setMovies(transformedMovies);
+    //   });
   }
 
   return (
