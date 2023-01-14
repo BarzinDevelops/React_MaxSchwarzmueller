deb0e361ea16cd49838753914e712ff045d47dce
Barzin <barzin.development@gmail.com>
2023-01-14 19:04:49 +0100

    Section 18: Redux and React => Add a new button ('increase by 5') and setup the handler function wich dispatches 'increase' action type with a payload (in this case 'amount'). Using payload as exptra object property makes it possible to dynamically increase the reducer by any number. So therefore create a new condition in reducer that handles 'increase' action types.

--- Sec18b_ReduxAndReact/src/components/Counter.js
+++ Sec18b_ReduxAndReact/src/components/Counter.js
@@ -1,17 +1,21 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import classes from './Counter.module.css';
-
+import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state=> state.counter);
+  const counter = useSelector((state) => state.counter);
 
-  const incrementHandler = () =>{
-    dispatch({type: 'increment'});
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
   };
-  const decrementHandler = () =>{
-    dispatch({type: 'decrement'});
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
   };
 
   const toggleCounterHandler = () => {};
@@ -22,6 +26,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
